b647732c8f99362a8cb5d27bb5eb8fc7fcbffadb
amalgamallama <svetlana.svinina@gmail.com>
2024-01-27 02:41:25 +0300

    Реализует слайдер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -14,3 +14,31 @@ navToggle.addEventListener("click", function() {
     nav.classList.remove("nav--opened");
   }
 });
+
+// слайдер
+const slider = document.querySelector(".demo-slider")
+const control = slider.querySelector(".demo-slider__control")
+let sliderStyles = getComputedStyle(slider)
+let controlPositionStart
+let clientX
+
+slider.classList.remove("demo-slider--no-js")
+window.addEventListener("pointerup", stopControlShifting)
+control.addEventListener("pointerdown", startControlShifting)
+
+function startControlShifting (event) {
+	controlPositionStart = +(sliderStyles.getPropertyValue("--control-position"))
+	clientX = event.clientX
+	window.addEventListener("pointermove", shiftControl)
+}
+
+function shiftControl (event) {
+	let deltaX = event.clientX - clientX
+	let cursorPlace = controlPositionStart + deltaX / slider.clientWidth
+	let controlPosition = Math.min(Math.max(cursorPlace, 0), 1)
+	slider.style.setProperty("--control-position", `${controlPosition}`)
+}
+
+function stopControlShifting () {
+	window.removeEventListener("pointermove", shiftControl)
+}
