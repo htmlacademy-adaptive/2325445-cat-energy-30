942a27ee767f8eb98b1cba041472bbff7bc852ba
amalgamallama <svetlana.svinina@gmail.com>
2024-01-26 04:11:48 +0300

    Реализует выпадающее меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
-/* в этот файл добавляет скрипты*/
+// выпадающее меню
+const nav = document.querySelector(".nav");
+const navToggle = document.querySelector(".nav__toggle");
+
+nav.classList.remove("nav--no-js");
+nav.classList.add("nav--closed");
+navToggle.addEventListener("click", function() {
+  if (nav.classList.contains("nav--closed")) {
+    nav.classList.remove("nav--closed");
+    nav.classList.add("nav--opened");
+  }
+  else {
+    nav.classList.add("nav--closed");
+    nav.classList.remove("nav--opened");
+  }
+});
